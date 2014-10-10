c067c6f284e78bcf47bffdedf9bae7da58aecb03
Murat ARI <muratari@outlook.com>
2014-10-10 10:56:44 +0300

    Some beautifying work...

--- HW-01/src/simple.scala
+++ HW-01/src/simple.scala
@@ -3,13 +3,5 @@
  */
 object simple extends App{
   val simpleArray = Array[Int](10, 25, 30)
-
-  simpleArray.foreach( (arg:Int) => println(
-  {
-    if(arg % 2 == 0){
-      arg * 2
-    }else{
-      arg * 3
-    }
-  }))
+  simpleArray.foreach( (arg:Int) => println( if(arg % 2 == 0) arg * 2 else arg * 3 ) )
 }
